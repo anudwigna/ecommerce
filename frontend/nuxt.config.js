f7213201159370900bc6944f7de615e0ce59066c
Abhinandan Aryal <anudwigna@gmail.com>
2022-11-19 23:02:05 +0100

    on progress

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -41,8 +41,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@storefront-ui/vue/styles.scss',
-    'assets/main.css'
+    'assets/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,39 +2,69 @@
   <div>
     <SfHero>
       <SfHeroItem
-        title="Colorful summer dresses are already in store"
-        subtitle="Summer Collection 2019"
+        title="Happytreats is my name."
         button-text="Learn more"
         image="images/dog_1.jpg"
       />
       <SfHeroItem
-        title="Colorful summer dresses are already in store"
-        subtitle="Summer Collection 2019"
+        title="The one store that makes me feel happy"
         button-text="Learn more"
         image="images/dog_2.jpg"
       />
       <SfHeroItem
         title="Colorful summer dresses are already in store"
-        subtitle="Summer Collection 2019"
         button-text="Learn more"
         image="images/dog_3.jpg"
       />
     </SfHero>
-    <p v-for="item in products.items" :key="item.id">{{ item.name }}</p>
+    <div class="sf-heading" style="margin-top: 2%">
+      <h2 class="sf-heading__title h2">Latest Products</h2>
+    </div>
+    <v-container>
+      <v-row>
+        <v-col v-for="product in products.items" :key="product.id" cols="3">
+          <SfProductCard
+            :title="product.name"
+            :image="product.assets[0].preview"
+            :show-add-to-cart-button="true"
+            :score-rating="4"
+            :max-rating="5"
+            :regular-price="500"
+            :special-price="450"
+          ></SfProductCard>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <SfCallToAction
+            title="Opening Offers are in row."
+            button-text="Check Out Offers"
+            description="Receive special offers and gifts for our customers."
+            background="#424242"
+            image="images/doggy.jpeg"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import { SfHero } from '@storefront-ui/vue';
+import { SfHero, SfCallToAction, SfProductCard } from '@storefront-ui/vue';
 import { gql } from 'graphql-tag';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
-    products {
+    products(options: { take: 4, sort: { id: DESC } }) {
       totalItems
       items {
-        name
         id
+        name
+        slug
+        assets {
+          name
+          preview
+        }
       }
     }
   }
@@ -44,10 +74,12 @@ export default {
   name: 'IndexPage',
   components: {
     SfHero,
+    SfCallToAction,
+    SfProductCard,
   },
   data: function () {
     return {
-      products: { items: [] },
+      products: { items: [{ assets: [{ name: '' }] }] },
     };
   },
   apollo: {
